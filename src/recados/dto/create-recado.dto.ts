cc18c5e5917fdc1394078442d5518a50ba007d7f
Hugo Messias <hugomessias9@gmail.com>
2024-12-05 21:31:40 -0300

    Implementada validação de dados nos DTOs

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(255)
   readonly recado: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(50)
   readonly from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(50)
   readonly to: string;
 }
--- src/recados/dto/update-recado.dto.ts
+++ src/recados/dto/update-recado.dto.ts
@@ -1,6 +1,36 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UpdateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(255)
+  @IsOptional()
   readonly recado?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(50)
+  @IsOptional()
   readonly from?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(50)
+  @IsOptional()
   readonly to?: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  @IsOptional()
   readonly seen?: boolean;
 }
